6f6f801f9a42b27bd04d82b85118504d94936b8d
david1542 <dudu5321@hotmail.com>
2019-01-29 10:25:32 +0200

    bug fix

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const app = express()
 
+const forceHttps = function (req, res, next) {
+  if (process.env.NODE_ENV === 'production') {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url)
+    }
+  }
+  next()
+}
+
+app.use(forceHttps)
 app.use(serveStatic(__dirname + '/dist')) // eslint-disable-line
 
 const port = process.env.PORT || 5000
